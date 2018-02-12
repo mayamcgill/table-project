d14bbafa0feeb3365682af024cb450e816c68c0e
mayamcgill <mcgillmaya@gmail.com>
2018-02-12 15:22:33 -0800

    got nothing done

---
+++ src/TablePanel.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class TablePanel extends JPanel{
+    String[][] colors;
+    //width and height are both 12!!
+    //stealing code from lifepanel to draw a 12x12 grid.
+
+}
